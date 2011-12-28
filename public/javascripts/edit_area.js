fea5d4890ca26c13fd817e5e58de913fc87dfd66
Franzé Jr <franzejr@gmail.com>
2011-12-27 23:26:36 -0300

    The Layout has been chaged to the second version

---
+++ public/javascripts/edit_area.js
@@ -0,0 +1,41 @@
+editAreaLoader.init({
+			id : "textarea_1"		
+			,syntax: "xml"			
+			,start_highlight: true		
+			,EA_load_callback: "editAreaLoaded"
+			,show_line_colors: true
+			,allow_toggle: false
+			,toolbar: "new_document, save, load, |, |, search, go_to_line, |, undo, redo, |, select_font, |, change_smooth_selection, highlight, reset_highlight, |, help"
+			,load_callback: "my_load"
+			,save_callback: "my_save"
+
+		});
+
+		function editAreaLoaded(id){
+			if(id=="textarea_1")
+			{
+				open_file1();
+				open_file2();
+			}
+		}
+		
+		function open_file1()
+		{
+			var new_file= {id: "to\\ é # € to", text: "$authors= array();\n$news= array();", syntax: 'php', title: 'beautiful title'};
+			editAreaLoader.openFile('example_2', new_file);
+		}
+		
+		function open_file2()
+		{
+			var new_file= {id: "Filename", text: "<a href=\"toto\">\n\tbouh\n</a>\n<!-- it's a comment -->", syntax: 'html'};
+			editAreaLoader.openFile('example_2', new_file);
+		}
+		
+		// callback functions
+		function my_save(id, content){
+			alert("Here is the content of the EditArea '"+ id +"' as received by the save callback function:\n"+content);
+		}
+		
+		function my_load(id){
+			editAreaLoader.setValue(id, "The content is loaded from the load_callback function into EditArea");
+		}
