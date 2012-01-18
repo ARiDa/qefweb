d624dd926d65e9cc46ee6c1d1dcadaa7f01eb9e3
Regis Pires Magalhaes <regispiresmag@gmail.com>
2012-01-18 18:01:48 -0300

    added bindleftjoin and some more QEPs

--- codims-home_/Scripts/codimsEnv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ..
-cd ..
-java -classpath $GLOBUS_LOCATION/lib/derby-10.4.1.3.jar:/srv/QEF/CoDIMS/build/classes ch.epfl.CodimsEnv
--- codims-home_/Scripts/startMachine.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh douglas@$1 'source /etc/profile; killall java; export CODIMS_HOME="/srv/QEF/CoDIMS-tcp/build/classes/codims-home"; export GLOBUS_LOCATION="/srv/Globus"; export GLOBUS_TCP_PORT_RANGE=40000,40100; export PATH=${GLOBUS_LOCATION}/bin:$PATH; globus-start-container -nosec -p' $2 
